1afcb23883dae7d96bc4443a2c78b0692902e733
Gabriel Bohn <gabrielcbohn@gmail.com>
2020-04-13 15:28:49 -0300

    Added recipe model and routes

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const Serie = require("../models/serie");
+const Recipe = require("../models/recipe");
 const ensureLogin = require("connect-ensure-login");
 
 router.get("/", (req, res, next) => {
-  res.render("index", {user: req.user});
+  res.render("index", { user: req.user });
 });
 
 // Series routes
 router.get("/series", (req, res, next) => {
   Serie.find()
     .then((series) => {
-    //   console.log(series);
+      //   console.log(series);
       res.render("serie/series", { series, user: req.user });
     })
     .catch((error) => console.log(error));
@@ -50,7 +51,7 @@ router.post(
 
 // ADD SERIE
 router.get("/add-serie", (req, res, next) => {
-  res.render("serie/add-serie", {user: req.user});
+  res.render("serie/add-serie", { user: req.user });
 });
 
 router.post("/add-serie", (req, res, next) => {
@@ -65,7 +66,62 @@ router.post("/add-serie", (req, res, next) => {
 });
 
 // nome do cantor - dia/hora - link - estilo musical
-// titulo - rating - categoria - sinopse
-// receita - categoria - dificuldade - tempo
+
+// RECIPES ROUTES
+router.get("/receitas", (req, res, next) => {
+  Recipe.find()
+    .then((receitas) => {
+      //   console.log(receitas);
+      res.render("recipes/recipes", { receitas, user: req.user });
+    })
+    .catch((error) => console.log(error));
+});
+
+// RECIPE INFO
+router.get("/receita/:id", (req, res, next) => {
+  const { id } = req.params;
+
+  Recipe.findById(id)
+    .then((recipe) => {
+      res.render("recipes/recipe-detail", { recipe, user: req.user });
+    })
+    .catch((error) => console.log(error));
+});
+
+router.post(
+  "/recipe-review/:id",
+  ensureLogin.ensureLoggedIn(),
+  (req, res, next) => {
+    const { id } = req.params;
+    const { comment } = req.body;
+    // console.log(req.user.username)
+    Recipe.findByIdAndUpdate(
+      id,
+      { $push: { review: { user: req.user.username, comment } } },
+      { new: true }
+    )
+      .then((response) => {
+        console.log(response);
+        res.redirect(`/receita/${id}`);
+      })
+      .catch((error) => console.log(error));
+  }
+);
+
+// ADD RECIPE
+router.get("/add-receita", (req, res, next) => {
+  res.render("recipes/add-recipe", { user: req.user });
+});
+
+router.post("/add-recipe", (req, res, next) => {
+  const { name, duration, category, prepare, level } = req.body;
+
+  Recipe.create({ name, duration, category, prepare, level })
+    .then((response) => {
+      console.log(response);
+      res.redirect("/receitas");
+    })
+    .catch((error) => console.log(error));
+});
 
 module.exports = router;
